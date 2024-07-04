540837240ee3d2ed504a62c3c131ef225fade5a1
Thinh <lexuanthinh01@gmail.com>
2024-07-04 16:34:50 +0700

    vertion update hết model trước khi view chạy xong

--- Assets/_Game/Scipts/GamePlay/CollectBox.cs
+++ Assets/_Game/Scipts/GamePlay/CollectBox.cs
@@ -5,6 +5,7 @@ using System.Runtime.ExceptionServices;
 using UnityEngine;
 using DG.Tweening;
 using Unity.VisualScripting;
+using System;
 public class CollectBox : MonoBehaviour
 {
     [SerializeField] private Vector3 firstTilePos= new Vector3(0.65f,0.2f,0f);
@@ -12,11 +13,12 @@ public class CollectBox : MonoBehaviour
     [SerializeField] private Tile tilePrefab;
     [SerializeField] private int idTileInsert= 0;
     [SerializeField] private bool isNotAutoInsert= false;
+    [SerializeField] private bool isHighSpeed= false;
     [SerializeField] private int maxCollectTile= 8 ;
     private Transform myTransform;
     private int dem = 0;
     private List<Tile> listCurrentTile = new List<Tile>();
-    private List<Tile> listMatchTile = new List<Tile>();
+    private int demhighspeed = 0 ;
     private void Start()
     {
         myTransform = this.transform;
@@ -28,34 +30,58 @@ public class CollectBox : MonoBehaviour
         {
             Tile newTile =  Instantiate(tilePrefab,myTransform);
             newTile.transform.localPosition = new Vector3(3,10,0);
-            if (isNotAutoInsert)
-                newTile.setTile(idTileInsert);
-            else 
-                newTile.setTile();
+            //if (isNotAutoInsert)
+            //    newTile.setTile(idTileInsert);
+            //else 
+            //    newTile.setTile();
             StartCoroutine(AddTile(newTile));
-            Invoke(nameof(Test), 0.5f);
+            if (isHighSpeed)
+            {
+                demhighspeed++;
+            }
+            if (demhighspeed == 15)
+            {
+                isHighSpeed = false;
+                demhighspeed = 0;
+            }
+            if (dem < 3)
+            {
+                newTile.setTile(0);
+            }
+            else
+            if (dem < 6)
+            {
+                newTile.setTile(1);
+            }
+            else {
+                newTile.setTile();
+            }
+            Invoke(nameof(Test), isHighSpeed? 0.25f :  1f);
         }
         dem++;
     }
     private IEnumerator AddTile(Tile newTile)
     {
-        if (listCurrentTile.Count == maxCollectTile) yield return null;
-        yield return new WaitForSeconds(0.5f);
+        //if (listCurrentTile.Count == maxCollectTile) yield return null;
+        yield return new WaitForSeconds(0.2f);
         int insertIndex= listCurrentTile.Count;
-        InsertTile(newTile, ref insertIndex);
-        yield return new WaitForSeconds(0.8f);
+        MoveToInsertTile(newTile, ref insertIndex);
         if (CheckThreeMatch(insertIndex, newTile))
         {
-            HideMatchTile(insertIndex);
-        }
-        yield return new WaitForSeconds(0.25f);
-        if ( CheckThreeMatch(insertIndex, newTile) ) {
-            MoveUpTiles(insertIndex);
-
+            Dictionary<Tile, Vector3> dicTileToTarget = new Dictionary<Tile, Vector3>();
+            List<Tile> listMatchTile = new List<Tile>();
+            Debug.Log("is three match");
+            MoveUpTilesModel(insertIndex, dicTileToTarget, listMatchTile);
+            yield return new WaitForSeconds(0.7f);
+            HideMatchTile(listMatchTile);
+            yield return new WaitForSeconds(0.2f);
+            Debug.Log("MoveUpTilesView,RemoveMatchTile");
+            MoveUpTilesView(dicTileToTarget);
+            RemoveMatchTile(listMatchTile);
         }
         yield return null;
     }
-    private int InsertTile(Tile tile, ref int insertIndex)
+    private int MoveToInsertTile(Tile tile, ref int insertIndex)
     {
         for (int i = listCurrentTile.Count - 1; i >= 0; i--)
         {
@@ -66,59 +92,62 @@ public class CollectBox : MonoBehaviour
             }
         }
         listCurrentTile.Add(null);
-        if (insertIndex != listCurrentTile.Count - 1)
+        if (insertIndex != listCurrentTile.Count-1)
         {
             for (int i = listCurrentTile.Count - 2; i >= insertIndex; i--)
             {
+                listCurrentTile[i].transform.DOMove(myTransform.TransformPoint(firstTilePos + tilesDistance*(i+1)), 0.5f);
                 listCurrentTile[i + 1] = listCurrentTile[i];
-                listCurrentTile[i].transform.DOMove(listCurrentTile[i].transform.position + tilesDistance, 0.5f);
             }
         }
-        tile.transform.DOMove(myTransform.TransformPoint(firstTilePos + insertIndex * tilesDistance), 0.75f);
+        tile.transform.DOMove(myTransform.TransformPoint(firstTilePos + insertIndex * tilesDistance), 0.7f);
         listCurrentTile[insertIndex] = tile;
         return insertIndex;
     }
     private bool CheckThreeMatch(int insertIndex, Tile newTile)
     {
-
+        //return false; // test dont check three match
         if (insertIndex >= 2)
         {
             int newTileSpriteID = newTile.getSpriteID();
             if (listCurrentTile[insertIndex - 1].getSpriteID() != newTileSpriteID ||
                 listCurrentTile[insertIndex - 2].getSpriteID() != newTileSpriteID)
             {
-                Debug.Log("three is false");
                 return false;
             }
+            return true;
         }
         else
         {
             return false;
         }
-        return true;
     }
-    private void HideMatchTile(int newMatchIndex)
+    private void HideMatchTile(List<Tile> listMatchTile)
+    {
+        for (int i = 0; i <= 2; i++)
+        {
+            listMatchTile[i].gameObject.transform.DOScale(new Vector3(0, 0), 0.2f);
+        }
+    }
+    private void RemoveMatchTile(List<Tile> listMatchTile)
+    {
+        for (int i = 0; i <= 2; i++)
+        {
+            Destroy(listMatchTile[i].gameObject);
+            // sau này sẽ nâng cấp thành pooling
+        }
+        listMatchTile = new List<Tile>();
+    }
+    private void MoveUpTilesModel(int newMatchIndex,Dictionary<Tile, Vector3> dicTileToTarget, List<Tile> listMatchTile)
     {
         for (int i = newMatchIndex; i >= newMatchIndex - 2; i--)
         {
-            listCurrentTile[i].transform.DOScale(new Vector3(0,0), 0.2f);
             listMatchTile.Add(listCurrentTile[i]);
         }
-    }
-    private void RemoveMatchTile(int newMatchIndex)
-    {
-        for (int i = 0; i >= 2; i++)
-        {
-            Destroy(listMatchTile[i]);
-            // sau này sẽ nâng cấp thành pooling
-        }
-    }
-    private void MoveUpTiles(int newMatchIndex)
-    {
         for (int i = newMatchIndex + 1; i < listCurrentTile.Count; i++)
         {
-            listCurrentTile[i].transform.DOMove(listCurrentTile[i].transform.position - tilesDistance*3, 0.5f);
             listCurrentTile[i - 3] = listCurrentTile[i];
+            dicTileToTarget.Add(listCurrentTile[i - 3], myTransform.TransformPoint(firstTilePos + tilesDistance * (i - 3)));
         }
         int lastIndex = listCurrentTile.Count-1;
         for (int i = lastIndex; i >= lastIndex-2; i--)
@@ -126,4 +155,12 @@ public class CollectBox : MonoBehaviour
             listCurrentTile.RemoveAt(i);
         }
     }
+    private void MoveUpTilesView(Dictionary<Tile, Vector3> dicTileToTarget)
+    {
+        foreach(KeyValuePair<Tile,Vector3> tileToTarget  in dicTileToTarget)
+        {
+            tileToTarget.Key.transform.DOMove(tileToTarget.Value, 0.5f);
+        }
+    }
+
 }
